3490a5ce1acbf2c15bab3e04d6f5a9899e59a724
Kamie Short <kamie.short@gmail.com>
2022-03-24 13:13:00 -0700

    players displaying on team details page

--- src/views/Teams/TeamsDetails.js
+++ src/views/Teams/TeamsDetails.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { fetchTeamsById } from '../../services/fetchteams';
 import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function TeamsDetails() {
   const params = useParams();
-  const [teamdeets, setTeamDeets] = useState({});
+  const [teamdeets, setTeamDeets] = useState({ players: [] });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,7 +23,11 @@ export default function TeamsDetails() {
       <p>
         {teamdeets.city}, {teamdeets.state}
       </p>
-      <p>{teamdeets.player}</p>
+      {teamdeets.players.map((player) => (
+        <div key={player.id}>
+          {player.position}: <Link to={`players/${player.id}`}>{player.name}</Link>
+        </div>
+      ))}
     </div>
   );
 }
